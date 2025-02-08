fe06b6253d020a0620d3e9e0053cde82f631c38e
Oleksandr Semenenko <115368382+sashasemenenko@users.noreply.github.com>
2025-02-08 12:26:28 +0100

    variables

---
+++ variables.py
@@ -0,0 +1,13 @@
+name = 'Oleksandr'
+age = 37
+
+print(f"{name} is {age} years old.")
+
+age = 36
+print(name + " is " + str(age) +  " years old.")
+
+profession = 'software developer'
+print(f"{name} is {profession}.")
+
+Name = 'Dmytrii'
+print(f"{Name} is not {Name}.")
