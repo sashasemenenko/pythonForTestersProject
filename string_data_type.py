cede2effb4526cf8c8390e37b2492b1627e35325
Oleksandr Semenenko <oleksandr.semenenko@icloud.com>
2025-03-03 14:12:12 +0100

    strings

---
+++ string_data_type.py
@@ -0,0 +1,19 @@
+str_1 = "Hello, World!"
+str_2 = "Python is fun."
+
+print(str_1, str_2)
+
+print(str_1[1])
+print(f"The length of str_1 s {len(str_1)}.")
+
+new_str_1 = str_1.replace("o", "0")
+print(new_str_1)
+print(str_1.split(","))
+
+str_3 = str_1 + " " + str_2
+print(str_3)
+
+text = "Movie \"Home Alone\" is my favourite."
+print(text)
+text2 = "Movie 'Home Alone' is my favourite."
+print(text2)
