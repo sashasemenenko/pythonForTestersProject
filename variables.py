521675b3d9e14d9a18d5c2d336aab7f29e46a44a
Oleksandr Semenenko <oleksandr.semenenko@icloud.com>
2025-03-03 13:19:35 +0100

    variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+name = "Oleksandr"
+age = 37
+
+print(f"{name} is {age} years old.")
+
+age = 38
+print(f"{name} is {age} years old.")
+print(name + " is " + str(age) + " years old.") # Another way to print the statmnets.
+
+profession = 'software developer'
+print(f"{name} is {profession}.")
+
+# Variablse are case-sensitive.
+Name = "Nadiia"
+print(f"{Name} is not {name}.")
