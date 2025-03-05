6c7f6230fa35bf863d3826c2fc085056289dcd29
Oleksandr Semenenko <oleksandr.semenenko@icloud.com>
2025-03-05 12:55:01 +0100

    documentation

---
+++ documentation.py
@@ -0,0 +1,11 @@
+def add_numbers(a: int, b: int) -> int:
+    """
+    Add two numbers and return the result
+    :param a: The first number
+    :param b: The second numbers
+    :return: The sum of a nad b
+    """
+    return a + b
+
+
+print(add_numbers(3, 2))
