a38e3a2f7a3d5a62fcf802296dc524906a138d4d
Oleksandr Semenenko <oleksandr.semenenko@icloud.com>
2025-03-10 13:25:12 +0100

    practice Encapsulation in Python

---
+++ bank_account.py
@@ -0,0 +1,36 @@
+class BankAccount:
+    def __init__(self):
+        self.__balance = 0
+
+    @property
+    def balance(self):
+        return self.__balance
+
+    @balance.setter
+    def balance(self, new_balance):
+        if new_balance < 0:
+            raise ValueError("Balance can not be negative.")
+        self.__balance = new_balance
+
+    def deposit(self, amount):
+        if amount <= 0:
+            raise ValueError("Deposit amount can not be zero or negative.")
+        self.balance += amount
+
+    def withdrow(self, amount):
+        if amount <= 0:
+            raise ValueError("Withdrow amount can not be zero or negative.")
+        if amount > self.balance:
+            raise ValueError("Not enough funds!")
+        self.balance -= amount
+
+
+account = BankAccount()
+account.deposit(100)
+print(account.balance)
+
+account.withdrow(50)
+print(account.balance)
+
+account.withdrow(60)
+
